71237e23a71ecb840fff4824162eaf16232f5e4d
HypnoticSiege <65150090+HypnoticSiege@users.noreply.github.com>
2021-12-27 21:52:42 -0500

    I'm actually slow...

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     discord: {
-        token: "NzgxMTY0MjE4NjYxMzM5MTc2.X75p3w.GStgV70ZV98-lz5QTK7qBQbvJJo", //Get yours at https://discord.com/developers/applications/
+        token: "YOUR-TOKEN-HERE", //Get yours at https://discord.com/developers/applications/
 
         logs: {
             discord: {
@@ -22,7 +22,7 @@ module.exports = {
     },
 
     twitch: {
-        oauth: 'oauth:1sy6feu1mdrs3mktj7b6vqvx5o9x9n', //Get yours at https://twitchapps.com/tmi/
+        oauth: 'oauth:YOUR-OAUTH', //Get yours at https://twitchapps.com/tmi/
         channels: ['hypnoticsiege'],
         username: 'hypnoticsiege'
     },
